1b104ec1c04914c15feec3f16db3e7a0718a4192
“Kenneth2811” <kennymace43@gmail.com>
2022-09-09 05:06:08 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,22 @@
-#include <stdio.h>      
 
+#include <stdio.h>
 int main(void)                                                                                                                          
+
+                                                                                                                                        
 
 {                                                                                                                                       
 
+                                                                                                                                        
+
     puts("\"Programming is like building a multilingual puzzle");                                                                       
+
+                                                                                                                                        
 
     retun(0);                                                                                                                           
 
-}         
+                                                                                                                                        
+
+}              
+
+               
 
