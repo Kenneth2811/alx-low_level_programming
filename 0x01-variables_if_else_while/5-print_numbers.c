06949400604d8bfd723bbdb2cf30e1f8528ee036
“Kenneth2811” <kennymace43@gmail.com>
2022-09-11 12:32:11 -0700

    Task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the numbers
+ * Description: using the main function
+ * this program prints the numbers
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
