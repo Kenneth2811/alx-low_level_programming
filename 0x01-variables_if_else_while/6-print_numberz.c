987a256c9011e0d9f0fd127909722152070d2a63
“Kenneth2811” <kennymace43@gmail.com>
2022-09-12 13:18:24 -0700

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- entry point 
+ * Description: using gthe main function
+ * this program prints numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int d;
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+		putchar('\n');
+	return (0);
+}
