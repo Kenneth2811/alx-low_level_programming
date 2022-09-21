cb480a3b671f20ef5c8b535ce7ff54e602c6f6f1
“Kenneth2811” <kennymace43@gmail.com>
2022-09-21 15:24:38 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
- *_s *@dest: string that will be appendedhhhjjjjjjjjjjjjjjjjj
- *@src: string to be concatenated uponjjjjjjjjjjjjjjjjjjj
- *@n: string gggggggggggggggggggggggggggggggggggggggggggg
- *Return: return pointer to @dest
+ *_strncat - concatenates two strings but add inputed number of bytes
+ *@dest: string to be appended upon
+ *@src: string to be completed at end of dest
+ *@n: integer parameter to compare index to
+ *Return: returns new concatenated string
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
