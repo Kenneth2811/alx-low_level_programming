afec9032eb78d1701a36a5badb3a1c753d660dfe
“Kenneth2811” <kennymace43@gmail.com>
2022-09-22 02:57:17 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,24 @@
 #include "main.h"
-
 /**
- * leet - encodes a string into 1337
- * @s: string to encode
- *
- * Return: address of s
+ *leet - concatenates the string pointed to by @src to
+ *the end of the string pointed to by @dest
+ *@char: string that will be appendedhhh
+ *@str: string to be concatenated upon
+ * Return: return pointer to @dest
  */
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i, j;
-	char a[] = "aAeEoOtTlL"
-	char b[] = "4433007711";
+	int index1 = 0, index2;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
-	for (i = 0; *(s + i); i++)
+	while (str[++index1])
 	{
-		for (j = 0; j <= 9; j++)
+		for (index2 = 0; index2 <= 7; index2++)
 		{
-			if (a[j] == *(s + i))
-				*(s + i) = b[j];
+			if (str[index1] == leet[index2] ||
+			str[index1] - 32 == leet[index2])
+				str[index1] = index2 + '0';
 		}
 	}
-	return (s);
+	return (str);
 }
