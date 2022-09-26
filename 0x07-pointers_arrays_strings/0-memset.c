8e0eeedc8ba3788222aeb8a67590d623256bfb84
“Kenneth2811” <kennymace43@gmail.com>
2022-09-26 14:27:08 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,12 +3,11 @@
 
 /**
  *_memset - fills a memory block with a constant byte 
- * @s: address to memory block
- * @b: char to be used
- * @n: number of bytes to be used
- * Return: pointer to the memory block
+ *@s: address to memory block
+ *@b: char to be used
+ *@n: number of bytes to be used
+ *Return: pointer to the memory block
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 	while (n)
